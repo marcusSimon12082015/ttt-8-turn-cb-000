a108b8d6a597baa063d706fa8791eb337fb3e253
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-07 13:01:39 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,22 @@ require_relative 'is_valid_index.rb'
 require_relative 'position_taken.rb'
 
 def turn(board,input)
-
+  puts "What is your move?"
+	index = gets.strip.to_i
+	valid = nil
+	loop do
+		if valid_move?(board,index)
+			board[index-1] = "X"
+			display_board(board)
+			valid = true
+		else
+			puts "What is your move?"
+			index = gets.strip.to_i
+		end
+		if valid == true
+			break
+		end
+	end
 end
 
 def valid_move?(board,index)
