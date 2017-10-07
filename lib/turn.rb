6c1a33e7b1e6dcaea81bf98e4da12a57a0dfe74e
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-07 13:01:15 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,10 +2,10 @@ require_relative 'display_board.rb'
 require_relative 'is_valid_index.rb'
 require_relative 'position_taken.rb'
 
-def turn(input)
-  loop do
-    puts "Where would you like to go?"
-    value = gets.split
-    if valid_move?(board,input)
-  end
+def turn(board,input)
+
+end
+
+def valid_move?(board,index)
+  is_valid_index(index) && !position_taken?(board,index)
 end
